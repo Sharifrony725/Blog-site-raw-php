44964b73bb00947cc325c6b928483aba74b571c7
Rony <sharifrony725@gmail.com>
2021-04-29 23:07:58 +0600

    db config separation

--- db_config.php
+++ db_config.php
@@ -1,3 +1,8 @@
 <?php 
-$db_connection = mysqli_connect('localhost','root','','blog-site');
+require_once'db_credential.php';
+$db_connection = mysqli_connect($host,$user,$password,$db_name);
+if (!$db_connection) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
 ?>
